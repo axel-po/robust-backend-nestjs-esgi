d3f863fe24ace66bf476f39d7cf627a6b7c2c287
Axel Pointud <axel@leedpulse.com>
2026-04-18 11:17:33 +0200

    add; Swagger with Nestia

--- apps/api/scripts/swagger.ts
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-import path from 'path';
-
-const sdkPath = require.resolve('@nestia/sdk/lib/executable/sdk.js');
-execSync(`node ${sdkPath} swagger`, {
-  cwd: path.resolve(__dirname, '..'),
-  stdio: 'inherit',
-});
